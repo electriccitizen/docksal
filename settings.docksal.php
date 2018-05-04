50f60e888337170173fe88ac4c10035113b949c5
Tim Broeker <tim@electriccitizen.com>
2018-05-04 14:53:44 -0500

    docksal.settings

---
+++ settings.docksal.php
@@ -0,0 +1,9 @@
+<?php
+# Docksal DB connection settings.
+$databases['default']['default'] = array (
+  'database' => 'default',
+  'username' => 'user',
+  'password' => 'user',
+  'host' => 'db',
+  'driver' => 'mysql',
+);
