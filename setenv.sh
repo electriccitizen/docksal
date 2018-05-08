f6e284d1bb783382d4e18c41ef5d918580a35224
Tim Broeker <tim@electriccitizen.com>
2018-05-08 13:53:53 -0500

    fix sed command for linux

--- setenv.sh
+++ setenv.sh
@@ -7,7 +7,7 @@ PANTHEON_SITE_ENV=$1
 REMOTE_ALIAS=$2
 
 # Set up remote aliases/site for docksal-local.env
-sed -i '' 's/DOCROOT=.*/DOCROOT='$DOCROOT'/' $file
-sed -i '' 's/GIT_BRANCH=.*/GIT_BRANCH='$GITBRANCH'/' $file
-sed -i '' 's/PANTHEON_SITE_ENV=.*/PANTHEON_SITE_ENV='$PANTHEON_SITE_ENV'/' $file
-sed -i '' 's/REMOTE_ALIAS=.*/REMOTE_ALIAS='$REMOTE_ALIAS'/' $file
+sed -i'' 's/DOCROOT=.*/DOCROOT='$DOCROOT'/' $file
+sed -i'' 's/GIT_BRANCH=.*/GIT_BRANCH='$GITBRANCH'/' $file
+sed -i'' 's/PANTHEON_SITE_ENV=.*/PANTHEON_SITE_ENV='$PANTHEON_SITE_ENV'/' $file
+sed -i'' 's/REMOTE_ALIAS=.*/REMOTE_ALIAS='$REMOTE_ALIAS'/' $file
