2c6d12419b9534a1673c94c868c70397d4e04194
Tim Broeker <tim@electriccitizen.com>
2018-05-08 13:07:35 -0500

    re-add setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+file=docksal-local.env
+DOCROOT=web
+GITBRANCH=master
+PANTHEON_SITE_ENV=$1
+REMOTE_ALIAS=$2
+
+# Set up remote aliases/site for docksal-local.env
+sed -i '' 's/DOCROOT=.*/DOCROOT='$DOCROOT'/' $file
+sed -i '' 's/GIT_BRANCH=.*/GIT_BRANCH='$GITBRANCH'/' $file
+sed -i '' 's/PANTHEON_SITE_ENV=.*/PANTHEON_SITE_ENV='$PANTHEON_SITE_ENV'/' $file
+sed -i '' 's/REMOTE_ALIAS=.*/REMOTE_ALIAS='$REMOTE_ALIAS'/' $file
